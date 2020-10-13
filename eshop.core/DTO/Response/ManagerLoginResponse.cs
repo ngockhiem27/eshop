0a460a495edf28b37fa908c0065c2ac8e4590660
unknown <khiem2794@gmail.com>
2020-10-13 15:13:54 +0700

    fix: add json format

--- eshop.core/DTO/Response/ManagerLoginResponse.cs
+++ eshop.core/DTO/Response/ManagerLoginResponse.cs
@@ -1,13 +1,17 @@
 ﻿using eshop.core.ViewModels;
+using System.Text.Json.Serialization;
 
 namespace eshop.core.DTO.Response
 {
     public class ManagerLoginResponse
     {
+        [JsonPropertyName("manager")]
         public ManagerViewModel Manager { get; set; }
 
+        [JsonPropertyName("accessToken")]
         public string AccessToken { get; set; }
 
+        [JsonPropertyName("refreshToken")]
         public string RefreshToken { get; set; }
     }
 }

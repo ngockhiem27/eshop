af9e8d68a8d048712fe3ae5a1e306066b05dd764
unknown <ngockhiem27@gmail.com>
2020-10-21 17:43:14 +0700

    tweak: ref db connection

--- eshop.apiservices/Repositories/BaseRepositoty.cs
+++ eshop.apiservices/Repositories/BaseRepositoty.cs
@@ -1,26 +1,35 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Oracle.ManagedDataAccess.Client;
+using System;
 using System.Data;
+using System.Data.Common;
 
 namespace eshop.apiservices.Repositories
 {
-    public abstract class BaseRepositoty
+    public abstract class BaseRepositoty : IDisposable
     {
         private readonly IConfiguration configuration;
+        private DbConnection _dbConn;
 
         public BaseRepositoty(IConfiguration configuration)
         {
             this.configuration = configuration;
+            _dbConn = OracleClientFactory.Instance.CreateConnection();
+            _dbConn.ConnectionString = configuration.GetConnectionString("Oracle");
         }
 
         public IDbConnection GetOpenConnection()
         {
-            var conn = new OracleConnection(configuration.GetConnectionString("Oracle"));
-            if (conn.State == ConnectionState.Closed)
+            if (_dbConn.State == ConnectionState.Closed)
             {
-                conn.Open();
+                _dbConn.Open();
             }
-            return conn;
+            return _dbConn;
+        }
+
+        public void Dispose()
+        {
+            // _dbConn.Dispose();
         }
     }
 }
